da28426e67e908bba9f860ccbd405a7201dc910f
wxu <eagleinfly@gmail.com>
2012-04-05 22:16:31 +0800

    calculate default test duration automatically from the history test report, and set max seconds exceed averate to be test_duration/2

--- test_distributor.py
+++ test_distributor.py
@@ -51,12 +51,12 @@ class TestBlock:
             test.block = self
 
 class TestDistributor:
-    def __init__(self, history_test_time, default_test_duration=1, max_seconds_exceed_average=10):
+    def __init__(self, history_test_time):
         self.tests_with_time = list()
         self.blocks_with_time = list()
         self.history_test_time = history_test_time
-        self.default_test_duration = default_test_duration
-        self.max_seconds_exceed_average = max_seconds_exceed_average
+        self.default_test_duration = self._default_test_time(history_test_time)
+        self.max_seconds_exceed_average = self.default_test_duration/2
 
     def assign(self, total_blocks, tests):
         self._init_test_time(tests)
@@ -64,7 +64,7 @@ class TestDistributor:
         for block_idx in range(1, total_blocks + 1):
             count_of_tests, total_time, average_build_time = self._average_build_time(total_blocks - block_idx + 1, self.tests_with_time)
             max_build_time = average_build_time + self.max_seconds_exceed_average
-            print("Estimated total test time {0} seconds, average test time {1} for all {1} tests.".format(str(total_time), str(average_build_time), str(count_of_tests)))
+            print("Estimated total test time {0} seconds, average test time {1} for all {2} tests.".format(str(total_time), str(average_build_time), str(count_of_tests)))
             current_block  = TestBlock(str(block_idx), average_build_time, max_build_time, block_idx == total_blocks)
 
             for test in self.tests_with_time:
@@ -74,6 +74,9 @@ class TestDistributor:
             self.blocks_with_time.append(current_block)
 
         return self.blocks_with_time
+
+    def _default_test_time(self, history_test_time):
+        return sum(history_test_time.values()) / len(history_test_time) if len(history_test_time) > 0 else 2
 
     def _get_tests_time(self, test_name):
         if self.history_test_time.has_key(test_name):
@@ -98,7 +101,7 @@ class TestDistributor:
 
         return count_of_tests, total_time, total_time/blocks
 
-def parse_args():
+def parse_command_line_args():
     parser = argparse.ArgumentParser(description='Assign tests to n blocks for distributed tests.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
@@ -107,16 +110,16 @@ def parse_args():
     parser.add_argument('-p', '--name-prefix', dest='result_filename_prefix', nargs='?', help='base name for the result files', default="tests")
     parser.add_argument('-e', '--name-extension', dest='result_filename_extension', nargs='?', help='file extension for the result files', default='txt')
     parser.add_argument('-d', '--dest-folder', dest='dest_folder', nargs='?', help='destination folder for generated files', default='test-blocks')
-    parser.add_argument('--default-test-duration', dest='default_test_duration', type=int, nargs='?', help='destination folder for generated files', default=1)
-    parser.add_argument('--max-seconds-exceed-average', dest='max_seconds_exceed_average', type=int, nargs='?', help='max seconds exceed average test time for each block', default=10)
     parser.add_argument('--delimiter', dest='delimiter', type=str, nargs='?', help='delimiter', default=',')
 
     return parser.parse_args()
 
 if __name__ == '__main__':
-    args = parse_args()
-
+    args = parse_command_line_args()
     parser = JunitTestReportsParser()
+
+    if not os.path.exists(args.dest_folder):
+        os.makedirs(args.dest_folder)
 
     if not os.path.exists(args.tests_history_report_folder):
         print("INFO: History report folder doesn't exists, assigning tests by numbers")
@@ -125,11 +128,9 @@ if __name__ == '__main__':
 
     tests_to_assign = sys.stdin.readlines()
 
-    distributor = TestDistributor(parser.tests_with_time, args.default_test_duration, args.max_seconds_exceed_average)
+    distributor = TestDistributor(parser.tests_with_time)
+    print("Default test duration is: {0}".format(str(distributor.default_test_duration)))
     blocks = distributor.assign(args.blocks, tests_to_assign)
-
-    if not os.path.exists(args.dest_folder):
-        os.makedirs(args.dest_folder)
 
     delimiter = args.delimiter.decode('string_escape')
     for block in blocks:
